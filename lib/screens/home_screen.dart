893cd5e4312071892550271876251290dbd6ec26
MaheelLakshan <MaheelLakshanra@gmail.com>
2024-06-21 00:08:37 +0530

    app bar was designed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -36,10 +37,67 @@ class HomeView extends StatelessWidget {
             ),
             Text(
               "Maheel Lakshan",
-              style: textTheme.bodyLarge!.copyWith(),
+              style: textTheme.bodyLarge!.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.location_on,
+                  color: colorScheme.secondary,
+                ),
+                const SizedBox(
+                  width: 4,
+                ),
+                Text(
+                  'UOR',
+                  style: textTheme.bodySmall!
+                      .copyWith(color: colorScheme.secondary),
+                ),
+                const SizedBox(
+                  width: 4,
+                ),
+                Icon(
+                  Icons.expand_more,
+                  color: colorScheme.secondary,
+                )
+              ],
             )
           ],
         ),
+        actions: [
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.notifications_outlined)),
+          const SizedBox(
+            width: 8,
+          )
+        ],
+        bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(64),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Search for Doctors .....',
+                  prefixIcon: const Icon(Icons.search),
+                  suffixIcon: Container(
+                    margin: const EdgeInsets.all(4),
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: colorScheme.onSurfaceVariant,
+                        borderRadius: BorderRadius.circular(8.0)),
+                    child: Icon(
+                      Icons.filter_alt_outlined,
+                      color: colorScheme.surfaceVariant,
+                    ),
+                  ),
+                ),
+              ),
+            )),
       ),
     );
   }
